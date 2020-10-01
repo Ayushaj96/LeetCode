7edad54a4e227571e72c30c0188ae77c1a8fece0
Akv2021 <akv.abd@gmail.com>
2020-10-01 23:27:49 +0530

    added generic boilerplate'

---
+++ boilerPlate.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int myFunction(vector<int>& a) {
+        /* code */
+        return res;
+    }
+};
+
+int main() {
+
+    Solution sol;
+    vector<int> a; //{1,2};
+    // int num;
+    // int i = 0;
+    // while (!cin.eof()){
+    //     cin>>num;
+    //     a.push_back(num);
+    //     i++;
+    //     cout<<endl<<"Found :"<<i<<" -> "<<a[i-1];
+    // }
+    cout<<"Input length "<<a.size();
+    cout<<endl<<"Final soln "<<endl;
+    cout<<"res is : "<<sol.myFunction(a)<<endl;
+    return 0;
+}
